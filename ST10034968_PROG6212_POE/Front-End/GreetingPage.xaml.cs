c1b611c3b1095f611d5478d34684a3fb8d703ca5
Gabriella Janssen <ST10034968@vcconnect.edu.za>
2023-09-04 11:08:06 +0200

    added WPF window named "GreetingPage" and made the startupUri this page

--- ST10034968_PROG6212_POE/Front-End/GreetingPage.xaml.cs
+++ ST10034968_PROG6212_POE/Front-End/GreetingPage.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ST10034968_PROG6212_POE
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for GreetingPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class GreetingPage : Window
     {
-        public MainWindow()
+        public GreetingPage()
         {
             InitializeComponent();
         }
