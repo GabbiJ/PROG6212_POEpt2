dd417a341dae2f37ed18e0284b680b11c195db91
Gabriella Janssen <ST10034968@vcconnect.edu.za>
2023-09-05 21:42:46 +0200

    added all other WPF windows in front-end folder

---
+++ ST10034968_PROG6212_POE/Front-End/AddStudyTimeForm.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ST10034968_PROG6212_POE.Front_End
+{
+    /// <summary>
+    /// Interaction logic for AddStudyTimeForm.xaml
+    /// </summary>
+    public partial class AddStudyTimeForm : Window
+    {
+        public AddStudyTimeForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
