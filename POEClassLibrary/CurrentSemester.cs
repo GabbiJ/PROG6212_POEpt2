b820250856225a300ba4a6e903443a8d8049c268
Gabriella Janssen <ST10034968@vcconnect.edu.za>
2023-09-10 10:15:53 +0200

    added functionality to addModuleForm add button

--- POEClassLibrary/CurrentSemester.cs
+++ POEClassLibrary/CurrentSemester.cs
@@ -9,8 +9,7 @@ namespace POEClassLibrary
     public static class CurrentSemester
     {
         //Declarations
-        public static List<Module> modules = new List<Module>{new Module("PROG612", "Programming", 16, 10),
-                new Module("CLDV6221", "Cloud Computing", 16, 15) };
+        public static List<Module> modules = new List<Module>();
         public static List<StudyTime> selfStudyCompleted = new List<StudyTime>();
         public static int NumOfWeeks { get; set; }
         public static DateTime StartDate { get; set; }
--- ST10034968_PROG6212_POE/Front-End/AddModuleForm.xaml.cs
+++ ST10034968_PROG6212_POE/Front-End/AddModuleForm.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using POEClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,6 +28,39 @@ namespace ST10034968_PROG6212_POE.Front_End
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
             //adding inputted info into modules list in CurrentSemester class
+            try
+            {
+                //storing the values in temporary variables
+                string? modName = txbModName.Text;
+                string? modCode = txbModCode.Text;
+                int? numOfCredits = Convert.ToInt32(txbNumOfCredits.Text);
+                int? classHrsPerWeek = Convert.ToInt32(txbClassHoursPerWeek.Text);
+                //inputting module name
+                if (modName == null)
+                {
+                    throw new NullReferenceException("Please enter the module name\n");
+                }
+                else if (modCode == null)
+                {
+                    throw new NullReferenceException("Please enter the module code\n");
+                }
+                else if (numOfCredits == null)
+                {
+                    throw new NullReferenceException("Please enter the number of credits\n");
+                }
+                else if (classHrsPerWeek == null)
+                {
+                    throw new NullReferenceException("Please enter class hours per week\n");
+                }
+                else
+                {
+                    CurrentSemester.modules.Add(new Module(modCode, modName, (int)numOfCredits, (int)classHrsPerWeek));
+                }
+            }
+            catch (Exception ex)
+            {
+                lblError.Content = ex.Message;
+            }
             
 
         }
