76474b40f0fb86b6ee9e7b1ecabba13242d34d41
Gabriella Janssen <ST10034968@vcconnect.edu.za>
2023-09-11 09:21:39 +0200

    documenting HomeWindow backend code

--- ST10034968_PROG6212_POE/Front-End/HomeWindow.xaml.cs
+++ ST10034968_PROG6212_POE/Front-End/HomeWindow.xaml.cs
@@ -20,15 +20,22 @@ namespace ST10034968_PROG6212_POE.Front_End
     /// </summary>
     public partial class HomeWindow : Window
     {
+        /// <summary>
+        /// constructor for HomeWindow
+        /// </summary>
         public HomeWindow()
         {
             InitializeComponent();
             //assigning values to labels for start date and duration of the current semester
             lblDateValue.Content = $"{CurrentSemester.StartDate.Day.ToString()} {CurrentSemester.StartDate.ToString("MMMM")} {CurrentSemester.StartDate.Year.ToString()}";
             lblDurationValue.Content = CurrentSemester.NumOfWeeks;
+            //displaying all modules in list view object
             displayDataToListView();
         }
 
+        /// <summary>
+        /// Displays all the modules in the list in the CurrentSemester class to the list view object 
+        /// </summary>
         public void displayDataToListView()
         {
             //using a LINQ query to get modules from list in CurrentSemester class with an additional list
@@ -46,30 +53,50 @@ namespace ST10034968_PROG6212_POE.Front_End
         }
 
         
-
+        /// <summary>
+        /// Method that allows the application to terminate when the 'Exit' button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnExit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
-
+        /// <summary>
+        /// Method that opens the form to edit the current semester information when the 'Edit' button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnEdit_Click(object sender, RoutedEventArgs e)
         {
             SemesterInfoForm sif = new SemesterInfoForm();
             sif.Show();
         }
-
+        /// <summary>
+        /// Method that opens the form to add a module when the 'Add' button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
             AddModuleForm amf = new AddModuleForm();
             amf.Show();
         }
-
+        /// <summary>
+        /// Method that opens the form to add study time when the 'Add Study Time' button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnAddStudyTime_Click(object sender, RoutedEventArgs e)
         {
             AddStudyTimeForm asf = new AddStudyTimeForm();
             asf.Show();
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnRefresh_Click(object sender, RoutedEventArgs e)
         {
             displayDataToListView();
