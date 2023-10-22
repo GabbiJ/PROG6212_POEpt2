960e0a9a02693492809952654eba330d7fc82841
Gabriella Home PC <ST10034968@vcconnect.edu.za>
2023-10-22 13:10:02 +0200

    Initial commit for Part 2

--- ST10034968_PROG6212_POE/obj/Debug/net7.0-windows/App.g.i.cs
+++ ST10034968_PROG6212_POE/obj/Debug/net7.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace ST10034968_PROG6212_POE {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace ST10034968_PROG6212_POE {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/ST10034968_PROG6212_POE;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ST10034968_PROG6212_POE;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace ST10034968_PROG6212_POE {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
         public static void Main() {
             ST10034968_PROG6212_POE.App app = new ST10034968_PROG6212_POE.App();
             app.InitializeComponent();
