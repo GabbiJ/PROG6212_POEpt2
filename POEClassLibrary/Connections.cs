bab5ee726573056a4f9001a6f1aba87498c28f28
Gabriella Home PC <ST10034968@vcconnect.edu.za>
2023-10-27 11:10:04 +0200

    added database connection. added Student class

---
+++ POEClassLibrary/Connections.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POEClassLibrary
+{
+    internal class Connections
+    {
+        public static SqlConnection GetConnection()
+        {
+            string strCon = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=PROG6212_POEPART2;Integrated Security=True";
+            return new SqlConnection(strCon);
+        }
+    }
+}
---
+++ POEClassLibrary/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POEClassLibrary
+{
+    internal class Student
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        SqlConnection con = Connections.GetConnection();
+
+
+        public Student(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public void registerStudent(string username, string pass)
+        {
+            //hashing password
+            //the methods used to hash the password were done using mehotds found on https://stackoverflow.com/questions/4181198/how-to-hash-a-password
+           
+            //inserting data into database
+            using (con)
+                {
+                    string strInsert = $"INSERT INTO Student VALUES('{username}', '{pass}');)";
+                    con.Open();
+                    SqlCommand cmdInsert = new SqlCommand(strInsert, con);
+                    cmdInsert.ExecuteNonQuery();
+                }
+
+        }
+
+        public void login(string username, string pass)
+        {
+
+        }
+    }
+}
